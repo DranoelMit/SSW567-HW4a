35ca12f4d7bec638387f13e112a1126a2fd2dacc
DranoelMit <tim.matthew.leonard@gmail.com>
2021-03-15 13:57:07 -0400

    add mock to testing

--- test.py
+++ test.py
@@ -1,17 +1,22 @@
 import unittest
+from unittest import mock
 
-from GithubApi import fetchUserInfo
+import GithubApi
+
+@mock.patch('GithubApi.fetchUserInfo')
+def mock_fetchUserInfo(mock_fetch_fun):
+    mock_fetch_fun.return_value = ['Repo: AlexaDemo Number of commits: 1']
 
 class TestApi(unittest.TestCase):
 
   def testMissingParam(self):
-      self.assertEqual(fetchUserInfo(), 'Error: missing parameter uid (User ID)', 'User id is required')
+      self.assertEqual(GithubApi.fetchUserInfo(), 'Error: missing parameter uid (User ID)', 'User id is required')
   def testFirstRepo(self):
-      self.assertEqual(fetchUserInfo('DranoelMit')[0], 'Repo: AlexaDemo Number of commits: 1', 'First repo of user DranoelMit')
+      self.assertEqual(GithubApi.fetchUserInfo('DranoelMit')[0], 'Repo: AlexaDemo Number of commits: 1', 'First repo of user DranoelMit')
   def testInvalidUser(self):
-      self.assertEqual(fetchUserInfo('sdasdgasdjasdgjhasdgasdasgdasdascdasfcashd'), [], 'No repos for invalid user')
+      self.assertEqual(GithubApi.fetchUserInfo('sdasdgasdjasdgjhasdgasdasgdasdascdasfcashd'), [], 'No repos for invalid user')
   def testBadParam(self):
-      self.assertEqual(fetchUserInfo(5), 'User Id must be a string', 'Must pass function a string userId')    
+      self.assertEqual(GithubApi.fetchUserInfo(5), 'User Id must be a string', 'Must pass function a string userId')    
 
 
 if __name__ == '__main__':
